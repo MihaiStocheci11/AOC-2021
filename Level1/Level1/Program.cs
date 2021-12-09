a9bad347a3e0671a7d5ec4dd5eb0915ef3772ff5
Mihai Stocheci <mihaistocheci11@gmail.com>
2021-12-09 13:21:00 +0200

    Add project files.

---
+++ Level1/Level1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Level1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFile = @"C:\Users\Mihai\source\Repos\AOC\Level1\Level1\input.txt";
+            if (File.Exists(textFile))
+            {
+                // Read file using StreamReader. Reads file line by line  
+                using (StreamReader file = new StreamReader(textFile))
+                {
+                    int length = 0;
+                    string ln;
+                    int count = 0;
+                    var numbers = new List<int> { };
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        numbers.Add(Int32.Parse(ln));
+                        length++;
+                    }
+
+                    for (int i = 1; i < length; i++)
+                    {
+                        // check item before
+                        if (numbers[i] > numbers[i - 1])
+                        {
+                            count++;
+                        }
+                        file.Close();
+                        Console.WriteLine($"File has {length} lines.");
+                        Console.WriteLine(count);
+                    }
+                }
+            }
+        }
+    }
+}
